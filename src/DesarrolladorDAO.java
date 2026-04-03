93f9a1337bb37a25e4b59737c9c9876b310f205c
Daniel <danieladler326@gmail.com>
2026-04-04 00:19:27 +0200

    Creacion del metodo para modificar los datos de la tabla desarrollador

--- src/DesarrolladorDAO.java
+++ src/DesarrolladorDAO.java
@@ -71,4 +71,35 @@ public class DesarrolladorDAO {
             System.out.println(e.getMessage());
         }
     }
+
+    public static void actualizarDesarrollador(Desarrollador dev){
+
+        // Creacion de la sentencia SQL
+        String sentencia = "{CALL sp_update_desarrollador(?, ?, ?, ?, ?, ?, ?)}";
+
+        try {
+            // Conexion a la base de datos
+            Connection con = ConexionBD.getConnection();
+
+            // Preparacion de la sentencia
+            CallableStatement cs = con.prepareCall(sentencia);
+
+            // Se introducen todos los datos del usuario
+            cs.setInt(1, dev.getId());
+            cs.setString(2, dev.getDNI());
+            cs.setString(3, dev.getNombre());
+            cs.setString(4, dev.getApellido1());
+            cs.setString(5, dev.getApellido2());
+            cs.setString(6, dev.getEmail());
+            cs.setString(7, dev.getFecha_alta());
+
+            // Ejecuto la sentencia
+            cs.executeUpdate();
+
+            System.out.println("Modificacion completada correctamente");
+
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
